1c4936060a15d136d56c34aabe1ec094f1bad132
Commandcracker <49335821+Commandcracker@users.noreply.github.com>
2024-11-28 17:47:28 +0100

    Update veo

--- src/gucken/hoster/veo.py
+++ src/gucken/hoster/veo.py
@@ -4,6 +4,8 @@ from re import compile as re_compile
 from ..networking import AsyncClient
 from .common import DirectLink, Hoster
 
+
+REDIRECT_PATTERN = re_compile("https?://[^\s'\"<>]+")
 EXTRACT_VEO_HLS_PATTERN = re_compile(r"'hls': '(?P<hls>.*)'")
 
 
@@ -11,6 +13,11 @@ class VOEHoster(Hoster):
     async def get_direct_link(self) -> DirectLink:
         async with AsyncClient(verify=False) as client:
             response = await client.get(self.url)
-            match = EXTRACT_VEO_HLS_PATTERN.search(response.text)
-            link = match.group("hls")
-            return DirectLink(b64decode(link).decode())
+            match = REDIRECT_PATTERN.search(response.text)
+            link = match.group()
+
+            response2 = await client.get(link)
+            match2 = EXTRACT_VEO_HLS_PATTERN.search(response2.text)
+            link2 = match2.group("hls")
+
+            return DirectLink(b64decode(link2).decode())
