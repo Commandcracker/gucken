c418c6bf004f4eaa81fb4c89c7b670e828733fee
Commandcracker <49335821+Commandcracker@users.noreply.github.com>
2025-04-08 14:35:32 +0200

    fix VEO

--- src/gucken/hoster/veo.py
+++ src/gucken/hoster/veo.py
@@ -1,22 +1,38 @@
 from base64 import b64decode
 from re import compile as re_compile
+from ..utils import json_loads
 
 from ..networking import AsyncClient
 from .common import DirectLink, Hoster
 
 REDIRECT_PATTERN = re_compile("https?://[^'\"<>]+")
-EXTRACT_VEO_HLS_PATTERN = re_compile(r"'hls': '(?P<hls>.*)'")
 
+EXTRACT_VEO_HLS_PATTERN = re_compile(r"'hls': '(?P<hls>.*)'")
+HIDDEN_JSON_PATTERN = re_compile(r"var a168c='(?P<hidden_json>[^']+)'")
 
 class VOEHoster(Hoster):
     async def get_direct_link(self) -> DirectLink:
         async with AsyncClient(verify=False) as client:
-            response = await client.get(self.url)
-            match = REDIRECT_PATTERN.search(response.text)
-            link = match.group()
+            redirect_response = await client.get(self.url)
+            redirect_match = REDIRECT_PATTERN.search(redirect_response.text)
+            redirect_link = redirect_match.group()
 
-            response2 = await client.get(link)
-            match2 = EXTRACT_VEO_HLS_PATTERN.search(response2.text)
-            link2 = match2.group("hls")
+            response = await client.get(redirect_link)
 
-            return DirectLink(b64decode(link2).decode())
+            match = HIDDEN_JSON_PATTERN.search(response.text)
+            if match:
+                hidden_json = b64decode(match.group("hidden_json")).decode()
+                hidden_json = hidden_json[::-1]
+                hidden_json = json_loads(hidden_json)
+                hidden_json = hidden_json["source"]
+                return DirectLink(hidden_json)
+
+            hls_match = EXTRACT_VEO_HLS_PATTERN.search(response.text)
+            hls_link = hls_match.group("hls")
+            hls_link = b64decode(hls_link).decode()
+            return DirectLink(
+                url=hls_link,
+                # Requires "host", "origin" or "referer"
+                # can be "bypassed" by http get once for players without headers
+                headers = {"Referer": "https://nathanfromsubject.com/"}
+            )
